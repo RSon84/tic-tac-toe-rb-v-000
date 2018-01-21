f7d35255448e6c771a3161dbced4478c427b5dc9
Devin Wilson <craigvin84@yahoo.com>
2018-01-21 18:10:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,3 +36,11 @@ def position_taken?(board, index)
     return true
   end
 end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !(position_taken?(board, index))
+    return true
+  else !index.between?(0, 8) || (position_taken?(board, index))
+      return false
+  end
+end
