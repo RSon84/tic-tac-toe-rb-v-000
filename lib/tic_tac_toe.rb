af1cddccd23ea07d9ec52988c3f83fb1fefe0e91
Devin Wilson <craigvin84@yahoo.com>
2018-01-22 01:48:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,3 +75,42 @@ end
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
+
+def won?(board)
+
+empty_board = board.all? {|location| location == " "}
+WIN_COMBINATIONS.detect do |sub_array|
+ board[sub_array[0]] == board[sub_array[1]] && board[sub_array[1]] == board[sub_array[2]] && position_taken?(board, sub_array[0])
+  #if sub_array.all? { |value| board[value] =="X" } || sub_array.all? { |value| board[value] =="O" }
+  #    return sub_array
+  #elsif empty_board || full?(board)
+  #  return false
+  #  end
+
+end
+end
+
+
+def full?(board)
+  !board.any? { |location| location == " " }
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  if won?(board).nil?
+    return nil
+  end
+  if board[won?(board).first] == "X"
+    return board[won?(board).first]
+  elsif board[won?(board).first] == "O"
+return board[won?(board).first]
+
+  end
+end
